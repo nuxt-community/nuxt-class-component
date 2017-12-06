8425b611a1c22d405785c38847b981968c2b2f61
Clark Du <clark.duxin@gmail.com>
2017-12-06 16:48:50 +0800

    fix: rebuild the dist and upgrade dependencies

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,13 @@
 "use strict";
 exports.__esModule = true;
 var vue_class_component_1 = require("vue-class-component");
+exports["default"] = vue_class_component_1["default"];
+var vuex_class_1 = require("vuex-class");
+exports.State = vuex_class_1.State;
+exports.Getter = vuex_class_1.Getter;
+exports.Action = vuex_class_1.Action;
+exports.Mutation = vuex_class_1.Mutation;
+exports.namespace = vuex_class_1.namespace;
 vue_class_component_1["default"].registerHooks([
     'beforeRouteEnter',
     'beforeRouteLeave',
@@ -13,4 +20,3 @@ vue_class_component_1["default"].registerHooks([
     'scrollToTop',
     'validate'
 ]);
-exports["default"] = vue_class_component_1["default"];
