bb1d12809cd2a9473a8a32c91a27b3a6682e79b2
Quique Fdez Guerra <quique@ckgrafico.com>
2018-04-18 19:21:23 +0200

    Add augmented types for options
    Not sure if this is needed but I've detected tscheck errors in this code:
    
    ´´´
    import { Vue, Watch } from 'vue-property-decorator';
    import Component, { namespace, State, Action } from 'nuxt-class-component';
    
    @Component({
      components: {
        LoadingComponent,
        ChoicerComponent,
        StepperComponent
      },
      middleware: 'secured'
    })
    export default class extends Vue {
    ´´´
    
    And I've resolved it adding to my typings/vue.d.ts 
    ```
    declare module 'vue/types/options' {
      // Declare augmentation for Options
      interface ComponentOptions<
        V extends Vue,
        Data=DefaultData<V>,
        Methods=DefaultMethods<V>,
        Computed=DefaultComputed,
        PropsDef=PropsDefinition<DefaultProps>,
        Props=DefaultProps> {
    
        middleware: string;
      }
    }
    ```

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,17 @@
 import Component from 'vue-class-component';
 import { State, Getter, Action, Mutation, namespace } from 'vuex-class';
 export { Component as default, State, Getter, Action, Mutation, namespace };
+
+declare module 'vue/types/options' {
+  // Declare augmentation for Options
+  interface ComponentOptions<
+    V extends Vue,
+    Data=DefaultData<V>,
+    Methods=DefaultMethods<V>,
+    Computed=DefaultComputed,
+    PropsDef=PropsDefinition<DefaultProps>,
+    Props=DefaultProps> {
+
+    middleware: string;
+  }
+}
